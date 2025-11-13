6801d4a92a9ccfd718cab4f201ab18a1e22ca6f2
kittisolomon <officialkittisolomon@gmail.com>
2025-11-14 00:30:21 +0100

    added the product model and migration

--- db/migrate/20251113225815_create_products.rb
+++ db/migrate/20251113225815_create_products.rb
@@ -1,6 +1,12 @@
 class CreateProducts < ActiveRecord::Migration[8.1]
   def change
     create_table :products do |t|
+      t.string :name
+      t.decimal :price, precision: 10, scale: 2
+      t.integer :quantity
+      t.text :description
+      t.string :sku
+      t.boolean :instock, default: true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 0) do
+ActiveRecord::Schema[8.1].define(version: 2025_11_13_225815) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
+  create_table "products", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.text "description"
+    t.boolean "instock", default: true
+    t.string "name"
+    t.decimal "price", precision: 10, scale: 2
+    t.integer "quantity"
+    t.string "sku"
+    t.datetime "updated_at", null: false
+  end
 end
