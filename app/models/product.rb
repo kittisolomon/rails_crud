28c2d78d5a0de16db78a514fa90b82fbc0360959
kittisolomon <officialkittisolomon@gmail.com>
2025-11-14 02:53:38 +0100

    added controller

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,13 @@
 class Product < ApplicationRecord
+    validates :name, presence: true, uniqueness: true, length: {minimum:6, maximum:255}
+
+    validates :price, presence: true, numericality: {greater_than: 0}
+ 
+    validates :quantity, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+    validates :description, length: { maximum: 500 }, allow_blank: true
+
+    validates :sku, presence: true, uniqueness: true
+
+    validates :instock, inclusion: { in: [true, false] }
 end
