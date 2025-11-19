ff3e9789f06e6e6a38cfe4ce4f4a5e93ab5653e0
kittisolomon <officialkittisolomon@gmail.com>
2025-11-19 16:42:21 +0100

    implemented product update

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,17 +1,16 @@
 class ProductsController < ApplicationController
   def index
     @products = Product.page(params[:page]).per(5)
+    @products_count = Product.count
   end
 
   def new
     sku ="SKU-#{rand(10000000..99999999)}-#{Time.now.year}"
-
     @product = Product.new(sku: sku)
   end
 
   def create
     @product = Product.new(product_params)
-    
     if @product.save
       flash.now[:notice] = 'Product was successfully created.'
       @product = Product.new 
@@ -19,6 +18,31 @@ class ProductsController < ApplicationController
     render :new, status: @product.persisted? ? :ok : :unprocessable_entity
   end
 
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    
+    if @product.update(product_params)
+      flash[:notice] = 'Product updated successfully.'
+      redirect_to edit_product_path(@product)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    flash[:notice] = 'Product deleted successfully.'
+    redirect_to products_path
+  end
   private
 
   def product_params
